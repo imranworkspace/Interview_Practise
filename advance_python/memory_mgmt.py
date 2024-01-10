e5dad01fd511017b4c204b73e04bfb1e6f70d532
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-10 13:02:11 +0530

    memory mgmt example done

---
+++ advance_python/memory_mgmt.py
@@ -0,0 +1,24 @@
+def fun2(p):
+    q=p+10
+    return q
+    
+def fun1(x):
+    z=x+5
+    p=fun2(z)
+    return p
+
+x=5
+y=fun1(x)
+print(y)
+
+
+# stack   pri heap
+# x          5
+# y     
+# fun1->
+# z           10
+# p   
+# fun2->
+# q    .      20
+
+# output x=5 and y=20  z p (garbej collector also called dead object)
