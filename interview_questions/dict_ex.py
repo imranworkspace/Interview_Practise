2a989cac7d5b46ce05098536e49d56e147ba0638
imranworkspace <149460073+imranworkspace@users.noreply.github.com>
2024-01-25 01:15:52 +0530

    Update dict_ex.py

--- interview_questions/dict_ex.py
+++ interview_questions/dict_ex.py
@@ -1,9 +1,11 @@
-new_list = [(5, 'a'), (2, 'b'), (3, 'c'), (5, 'ab')]
+def list_to_dict(new_list):
+    d=dict()
+    for key,value in new_list:
+        if key not in new_list:
+            d[key]=value
+        else:
+            d[key]=value
+    return d
 
-key_dict={}
-for key,value in new_list:
-    if key not in key_dict:
-        key_dict[key]=[value]
-    else:
-        key_dict[key].append(value)
-print(key_dict)
+new_list = [(5, 'a'), (2, 'b'), (3, 'c'), (5, 'ab')]
+print(list_to_dict(new_list))
