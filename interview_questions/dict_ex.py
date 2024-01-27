54f67cfb8614cb3247f325fbba00124307546ca5
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-27 13:24:59 +0530

    mutltiprocessing and multithreading example

--- interview_questions/dict_ex.py
+++ interview_questions/dict_ex.py
@@ -1,7 +1,7 @@
 new_list = [(5, 'a'), (2, 'b'), (3, 'c'), (5, 'ab')]
 
 key_dict={}
-for key,value in new_list:
+for key,value in new_list: 
     if key not in key_dict:
         key_dict[key]=[value]
     else:
