360c575fe990237328f1d4c853d0041227099c2d
imranworkspace <149460073+imranworkspace@users.noreply.github.com>
2024-02-16 19:47:50 +0530

    Update fact_with_time_decorator.py

--- advance_python/fact_with_time_decorator.py
+++ advance_python/fact_with_time_decorator.py
@@ -1,21 +1,41 @@
 ################################
 # create factorial examle using decorator who calculate function execution time
 import time
-def time_decoreator(func):
-    def wrapper_fun(*args,**kwargs):
-        print('inside wrapper')
-        start=time.time()
-        end=time.time()
-        excution_time = end-start
-        print('excution_time ',excution_time)
+def time_decorator(func):
+    def wrapper(*args,**kwargs):
+        start_=time.time()
+        end_=time.time()
+        process_time = end_-start_
+        print('execution time ',process_time)
         return func
-    return wrapper_fun
-  
-@time_decoreator    
+    return wrapper()
+    
+@time_decorator
 def fact(n):
     if n==1:
         return 1
     return n*(fact(n-1))
+n=5
+print(f'fact of {n} of {fact(n)}')
+
+from math import factorial
+@time_decorator
+def fact2(n):
+    return factorial(n)
+n=5
+print(fact2(n))
+
+@time_decorator
+def fibo(n):
+    count,n1,n2=0,0,1
+    while count<n:
+        print(n1)
+        temp=n1+n2
+        n2=n1
+        n1=temp
+        count+=1
+n=5
+fibo(n)
 
 n=5
 print(f'fact of{n} is {fact(n)}')
