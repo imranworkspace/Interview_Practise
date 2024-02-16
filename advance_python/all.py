f4f1a08c5926748ffbb1920dbd651ef468a42be5
imranworkspace <149460073+imranworkspace@users.noreply.github.com>
2024-02-16 18:58:02 +0530

    Update all.py
    factorial with function execution decorator

--- advance_python/all.py
+++ advance_python/all.py
@@ -111,3 +111,29 @@ def prime():
         if i==j:
             print(i,end=',')
 print(prime())
+
+################################
+# create factorial examle using decorator who calculate function execution time
+import time
+def time_decoreator(func):
+    def wrapper_fun(*args,**kwargs):
+        print('inside wrapper')
+        start=time.time()
+        end=time.time()
+        excution_time = end-start
+        print('excution_time ',excution_time)
+        return func
+    return wrapper_fun
+  
+@time_decoreator    
+def fact(n):
+    if n==1:
+        return 1
+    return n*(fact(n-1))
+
+n=5
+print(f'fact of{n} is {fact(n)}')
+
+
+
+
