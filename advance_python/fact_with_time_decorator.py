b63f98c13e41a17519719cf74c2a27ee6e4600c5
imranworkspace <149460073+imranworkspace@users.noreply.github.com>
2024-02-26 19:37:25 +0530

    Update fact_with_time_decorator.py

--- advance_python/fact_with_time_decorator.py
+++ advance_python/fact_with_time_decorator.py
@@ -1,41 +1,31 @@
 ################################
 # create factorial examle using decorator who calculate function execution time
 import time
-def time_decorator(func):
+def time_decor(func):
     def wrapper(*args,**kwargs):
-        start_=time.time()
-        end_=time.time()
-        process_time = end_-start_
-        print('execution time ',process_time)
+        start=time.time()
+        end=time.time()
+        es=end-start
+        print(f'estimate time for {func.__name__}',es)
         return func
     return wrapper()
-    
-@time_decorator
-def fact(n):
-    if n==1:
-        return 1
-    return n*(fact(n-1))
-n=5
-print(f'fact of {n} of {fact(n)}')
 
-from math import factorial
-@time_decorator
-def fact2(n):
-    return factorial(n)
-n=5
-print(fact2(n))
-
-@time_decorator
+@time_decor
 def fibo(n):
     count,n1,n2=0,0,1
     while count<n:
         print(n1)
-        temp=n1+n2
-        n2=n1
-        n1=temp
+        n1,n2=n1+n2,n1
         count+=1
 n=5
 fibo(n)
 
-n=5
-print(f'fact of{n} is {fact(n)}')
+@time_decor
+def fprime():
+    for i in range(2,101):
+        for j in range(2,101):
+            if i%j==0:
+                break
+        if i==j:
+            print(i)
+fprime()
