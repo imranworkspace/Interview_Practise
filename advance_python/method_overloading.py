8432b2d5a3e3b33a46878e40b4c7bb19f7656e32
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-03-01 13:13:06 +0530

    add method overriding

--- advance_python/method_overloading.py
+++ advance_python/method_overloading.py
@@ -1,28 +1,34 @@
-# from multipledispatch import dispatch
+from multipledispatch import dispatch
 
-# @dispatch(int,int)
-# def myfun(a,b):
-#     print(a*b)
+@dispatch(int,int)
+def myfun(a,b):
+    print(a*b)
 
-# @dispatch(float,float,float)
-# def myfun(a,b,c):
-#     print(a*b*c)
-# myfun(1,2)
-# myfun(1.0,2.0,3.0)
+@dispatch(int,int,int)
+def myfun(a,b,c):
+    print(a*b*c)
 
-import multiprocessing
+@dispatch(float,float,float)
+def myfun(a,b,c):
+    print(a*b*c)
+myfun(1,2)
+myfun(1.0,2.0,3.0)
+myfun(1,2,3)
 
-def cube(a):
-    print(a*a*a)
-def sq(a):
-    print(a*a)
+# import multiprocessing
+
+# def cube(a):
+#     print(a*a*a)
+# def sq(a):
+#     print(a*a)
     
-p1=multiprocessing.Process(target=cube,args=(10,))
-p2=multiprocessing.Process(target=sq,args=(10,))
+# p1=multiprocessing.Process(target=cube,args=(10,))
+# p2=multiprocessing.Process(target=sq,args=(10,))
 
-p1.start()
-p2.start()
-p1.join()
-p2.join()
-print('done')
-print('all done')
+# p1.start()
+# p2.start()
+# p1.join()
+# p2.join()
+# print('done')
+# print('all done')
+
