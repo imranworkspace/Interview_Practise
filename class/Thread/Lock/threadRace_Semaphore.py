2f6b336ff11a7fbde0cc9868022b469ce6fab11e
imranlatur24 <imranlatur24@gmail.com>
2025-12-04 00:57:42 +0530

    locks

--- class/Thread/Lock/threadRace_Semaphore.py
+++ class/Thread/Lock/threadRace_Semaphore.py
@@ -1,31 +1,32 @@
-from threading import Thread,current_thread,Semaphore,BoundedSemaphore
+from threading import Thread, current_thread, Semaphore
+from time import sleep
 
 class Flight:
-    def __init__(self,available_seat):
-        self.available_seat = available_seat
-        # self.l = Semaphore(2) # semaphore - synchronize lock allow 2 threads lock at the same time  
-        self.l = BoundedSemaphore(2) # Bounded semaphore - synchronize lock allow 2 threads lock at the same time  
-    
-    def reserve(self,need_seat):
-        self.l.acquire() # locking from here 
-        # self.l._value() # show semaphore value 
-        print(f'available seat is {self.available_seat}')        
-        if self.available_seat >= need_seat:
-            name = current_thread().name 
-            print(f'need of seat {need_seat}, seat allowcated to {name}')
-            self.available_seat -= need_seat
-            print()
-        else:
-            print('-----')
-            print('sorry seats full..')
-        self.l.release() # realising lock from here 
+    def __init__(self, available_seats):
+        self.available_seats = available_seats
+        self.l = Semaphore(2)   # allow 2 threads at once
+        
+    def reserve(self, need_seat):
+        with self.l:   # safer than acquire/release
+            name = current_thread().name
+            print(f"{name} entered → available seats: {self.available_seats}")
+            
+            if self.available_seats >= need_seat:
+                print(f"Reservation confirmed for {name}")
+                self.available_seats -= need_seat
+                sleep(3)
+            else:
+                print(f"NOT enough seats for {name.upper()}")
+            
+            print(f"{name} leaving...")
 
-myt = Flight(2)
-t1 = Thread(target=myt.reserve,args=(1,),name="Irfan")
-t2 = Thread(target=myt.reserve,args=(1,),name="guddu")
-t3 = Thread(target=myt.reserve,args=(1,),name="shakil")
-
+f = Flight(3)
+t1 = Thread(target=f.reserve, args=(1,), name="Martodkar")
+t2 = Thread(target=f.reserve, args=(1,), name="Zunaisha")
+t3 = Thread(target=f.reserve, args=(1,), name="Afreen")
+t4 = Thread(target=f.reserve, args=(1,), name="Dilnaz")
 
 t1.start()
 t2.start()
 t3.start()
+t4.start()
