90fdcab1840af8d7d91b34415c3ba3acc0210038
imranlatur24 <imranlatur24@gmail.com>
2026-08-25 14:41:35 +0530

    update lst,dict

--- Python_Practise_Example/set_practise/set.py
+++ Python_Practise_Example/set_practise/set.py
@@ -1,15 +1,29 @@
 # months = {'imran'}
-months = set(["January","February", "March", "April", "May", "June"])    
+myset = set([8,9,10,11,12,1,2,3,4,5,6,7])    
 # discard() not raised any error if item is avaialble or not
-# months.discard("January");    
-# months.discard("Imran");    
+myset.discard(100)
+print(myset)   
 
 # remove() raised an error if item is avaialble or not 
 # raised an error KeyError: 'January'
-# months.remove("January");    
-# months.remove("Imran");    
+# myset.remove(100)
+# print(myset)
 
-months.clear()
+myset.add(100)
+print(myset)
 
-print(months)
-print(type(months))
+myset2=set()
+myset3=set()
+
+myset2=myset
+print('myset2',myset2)
+
+myset3=myset.copy()
+print('myset3',myset3)
+
+myset.pop()
+print('pop',myset)
+
+myset.remove(2)# 2 remove from set 
+print('pop',myset)
+
